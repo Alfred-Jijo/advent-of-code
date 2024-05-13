6d148730fb3b60a6574076840faaabb136881424
Alfred-Jijo <alfred.jijo28@gmail.com>
2024-05-13 19:59:37 +0100

    c lang day 03 p1 2015

--- c/2015/day02/day02.c
+++ c/2015/day02/day02.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 void extract_dimensions(char *str, int *length, int *width, int *height) {
     char *token = strtok(str, "x");
     *length = atoi(token);
@@ -14,28 +13,80 @@ void extract_dimensions(char *str, int *length, int *width, int *height) {
     *height = atoi(token);
 }
 
-int calc_area(int *length, int *width, int *height) {
-    int l = *length;
-    int w = *width;
-    int h = *height;
-    return ((2*l*w) + (2*w*h) + (2*h*l));
+int calc_area(int length, int width, int height) {
+    int lw = length * width;
+    int wh = width * height;
+    int hl = height * length;
+    return 2 * (lw + wh + hl);
+}
+
+int calc_extra(int length, int width, int height) {
+    int lw = length * width;
+    int wh = width * height;
+    int hl = height * length;
+    int smallest_side;
+    if (lw < wh) {
+        if (lw < hl) {
+            smallest_side = lw;
+        } else {
+            smallest_side = hl;
+        }
+    } else {
+        if (wh < hl) {
+            smallest_side = wh;
+        } else {
+            smallest_side = hl;
+        }
+    }
+    return smallest_side;
+}
+
+int calc_ribbon_length(int length, int width, int height) {
+    int smallest_perimeter;
+    if (length <= width && length <= height) {
+        if (width <= height) {
+            smallest_perimeter = 2 * (length + width);
+        } else {
+            smallest_perimeter = 2 * (length + height);
+        }
+    } else if (width <= length && width <= height) {
+        if (length <= height) {
+            smallest_perimeter = 2 * (width + length);
+        } else {
+            smallest_perimeter = 2 * (width + height);
+        }
+    } else {
+        if (length <= width) {
+            smallest_perimeter = 2 * (height + length);
+        } else {
+            smallest_perimeter = 2 * (height + width);
+        }
+    }
+    int volume = length * width * height;
+    return smallest_perimeter + volume;
 }
 
 int main(void) {
     FILE *f = fopen("puzzle_input.txt", "r");
-    if(f == NULL) {
+    if (f == NULL) {
         perror("Unable to open file!");
         exit(1);
     }
 
     char input[100];
-    int length, width, height, total, sq_ft;
+    int length, width, height, total_sq_ft = 0, total_ribbon_length = 0;
 
-    while(fgets(input, sizeof(input), f) != NULL) {
+    while (fgets(input, sizeof(input), f) != NULL) {
         extract_dimensions(input, &length, &width, &height);
-        /* printf("Length: %d, Width: %d, Height: %d\n", length, width, height); */
-        sq_ft = calc_area(&length, &width, &height);
-        total += sq_ft;
+        int area = calc_area(length, width, height);
+        int extra = calc_extra(length, width, height);
+        total_sq_ft += (area + extra);
+        int ribbon_length = calc_ribbon_length(length, width, height);
+        total_ribbon_length += ribbon_length;
     }
-    printf("Total Sqaure Feet: %d", total);
+    printf("Total Square Feet of Wrapping Paper: %d\n", total_sq_ft);
+    printf("Total Feet of Ribbon: %d\n", total_ribbon_length);
+
+    fclose(f);
+    return 0;
 }
