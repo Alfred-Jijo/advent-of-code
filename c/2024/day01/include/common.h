a76c62ae24bf18c684c7c8986e27afde2002697b
Alfred-Jijo <alfredjijo28@gmail.com>
2024-12-01 13:57:11 +0000

    incomplete example puzzle

---
+++ c/2024/day01/include/common.h
@@ -0,0 +1,41 @@
+#ifndef COMMON_H 
+#define COMMON_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+//! typedefs
+typedef intptr_t isize;
+typedef uintptr_t usize;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+
+typedef uint8_t byte;
+
+// size_t PAGE_SIZE = 0;
+
+#define KB (1024ULL)
+#define MB (KB * 1024)
+#define GB (MB * 1024)
+#define DEFAULT_ALIGNMENT (sizeof(void *) * 2)
+#define is_power_of_two(x) (x != 0 && (x & (x - 1)) == 0)
+
+#define bool  _Bool
+#define false 0
+#define true  1
+
+#endif // COMMOM_H
+
+
--- c/2024/day01/main.c
+++ c/2024/day01/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <common.h>
 
 int
-main (int argc, char **argv) {
-    return 0;
+compare (const void *, const void *);
+
+int
+calc_total_distance (int *left, int *right, int size);
+
+int
+main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("Usage: main.exe <puzzle_input> \n");
+	}
+
+	int left [] = { 3, 4, 2, 1, 3, 3 };
+	int right[] = { 4, 3, 5, 3, 9, 3 };
+	int size = sizeof(left) / sizeof(left[0]);
+
+	printf("T: %d\n", calc_total_distance(left, right, size));
+
+	return 0;
+}
+
+int
+compare (const void *a, const void *b) {
+	return (int *)a - (int *)b;
+}
+
+int
+calc_total_distance (int *left, int *right, int size) {
+	qsort(left, size, sizeof(int), compare);
+	qsort(right, size, sizeof(int), compare);
+
+	int total_distance, i;
+	total_distance = 0;
+
+	for (i = 0; i < size; i++) { 
+		total_distance += abs(left[i] - right[i]); 
+	}
+	return total_distance;
 }
